bc499b7b34eda102fd4199a3a3400cc6f23da5a1
minij147 <jakepaul628@gmail.com>
2024-12-20 00:38:55 -0500

    intilizer change

--- client/src/db.rs
+++ client/src/db.rs
@@ -1,8 +1,8 @@
 pub const QUERY_INIT: &str = " 
 CREATE TABLE sessions (
   id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-  name varchar(255) NOT NULL,
-  time_elapsed bigint NOT NULL,
+  name text NOT NULL,
+  time_elapsed bigint default 0 NOT NULL,
   time_current bigint NOT NULL,
   time_ended bigint default 0 NOT NULL,
   active int default 0 NOT NULL
@@ -24,3 +24,28 @@ CREATE TABLE notes(
   foreign_type int CHECK (foreign_type IN (0,1)) NOT NULL 
 );
 ";
+
+pub struct Session {
+    id: i32,
+    name: String,
+    time_elapsed: i64, 
+    time_current: i64,
+    time_ended: i64,
+    active: i8
+}
+
+pub struct Task {
+    id: i32,
+    name: String,
+    time_elapsed: i64,
+    time_current: i64,
+    active: i8,
+    completed: i8 
+
+}
+pub struct Note {
+    timestamp: u64,
+    message: String,
+    foreign_id: i8,
+    foreign_type: i8
+}
--- client/src/initializer.rs
+++ client/src/initializer.rs
@@ -3,20 +3,15 @@ use rusqlite::Connection;
 use crate::db; 
 
 fn get_abs_path() -> String {
-    std::env::var("HOME").expect("HOME enviroment variable not set")+"/.config/timetrack"
+    std::env::var("HOME").expect("HOME enviroment variable not set")+"/.timetrack.db"
 }
 
 pub fn verify() -> Result<(),String>{ 
     let abs_path = get_abs_path(); 
 
-    let exists = std::fs::exists(&abs_path).expect("failed to verify if ~/.config/timetrack/ exists");
-    if !exists {
-         return Err("~/.config/timetrack/ does not exists please run: timetrack init".to_string());
-    }
-
-    let exists_db = std::fs::exists(abs_path+"/data.db").expect("failed to verify if ~/.config/timetrack/data.db exists");
-    if !exists_db{
-        return Err("~/.config/timetrack/data.db does not exists please run: timetrack init".to_string());
+    let exists = std::fs::exists(&abs_path).expect("failed to verify if ~/.timetrack.db exists");
+    if !exists{
+        return Err("~/.timetrack.db does not exists please run: timetrack init".to_string());
     }
 
     Ok(())
@@ -25,16 +20,11 @@ pub fn verify() -> Result<(),String>{
 pub fn init() {    
     let abs_path = get_abs_path();
 
-    let exists = std::fs::exists(&abs_path).expect("failed to verify if ~/.config/timetrack/ exists");
-    if !exists{
-        std::fs::create_dir_all(&abs_path).unwrap();
-    }
-
-    let conn = Connection::open(abs_path+"/data.db").expect("failed to create data.db file with sqlite");
+    let conn = Connection::open(&abs_path).expect("failed to create data.db file with sqlite");
     conn.execute_batch(db::QUERY_INIT).expect("failed initilization query");
 }
 
 pub fn reset() {
     let abs_path = get_abs_path();
-    std::fs::remove_file(abs_path+"/data.db").expect("failed database drop");
+    std::fs::remove_file(abs_path).expect("failed database drop");
 }
