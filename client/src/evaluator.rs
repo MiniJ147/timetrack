3c329828b22c72efaeed17e6085a76a134aea0b8
minij147 <jakepaul628@gmail.com>
2024-12-26 02:53:44 -0500

    progress on evulator:

--- client/src/evaluator.rs
+++ client/src/evaluator.rs
@@ -1,15 +1,49 @@
-use crate::lexer;
+use crate::lexer::{Token,Type,Action};
 
-
-pub fn evaluate(tokens: &Vec<lexer::Token>) {
-    //value, keyword
-    let expected = lexer::Type::Value;
-    for token in tokens.iter() {
-        if token.kind.eq(&expected) {
-            println!("valued");
-        }else{
-            println!("invalid");
-        }
-    }
+#[derive(Debug)]
+pub struct State {
+    action: Action,
+    args: String,
+    value: String,
 }
 
+const SESSION_ARGS: [&str;12] = ["s","start","e","end","p","pause","v","view","m","message","t","time"];
+const TASK_ARGS: [&str;0] = []; // to be implemented 
+const LIST_ARGS: [&str;0] = []; // to be implemented
+
+pub fn evaluate(tokens: &Vec<Token>) {
+    //value, keyword
+    let mut state = State{action: Action::None, args: "".to_string(), value: "".to_string()}; 
+
+    let expected = vec![Type::Value, Type::Keyword, Type::Arg, Type::Value];
+    assert!(tokens.len() <= expected.len());
+
+    for (i, token) in tokens.iter().enumerate(){
+        if !token.kind.eq(&expected[i]) {
+            println!("invalid");
+            continue;
+        }
+        
+        match expected[i] {
+            Type::Keyword => state.action = token.action,
+            Type::Arg => {
+                let arg = token.str.replace("-","");
+
+                assert!(validate_arg(state.action, &arg[..]));
+                state.args = arg; 
+            },
+            Type::Value => state.value = token.str.clone(), 
+        }
+    }
+
+    println!("{:?}",state);
+}
+
+fn validate_arg(action: Action, arg_trimed: &str) -> bool {
+    match action {
+        Action::Session => SESSION_ARGS.contains(&arg_trimed),
+        Action::Task => TASK_ARGS.contains(&arg_trimed), 
+        Action::List => LIST_ARGS.contains(&arg_trimed),
+        Action::None => false,
+    }
+}
--- client/src/lexer.rs
+++ client/src/lexer.rs
@@ -6,9 +6,18 @@ pub enum Type {
     Value,
 }
 
+#[derive(Clone, Copy, Debug)]
+pub enum Action{
+    Session,
+    Task,
+    List,
+    None 
+}
+
 pub struct Token {
     pub kind: Type,
     pub str: String,
+    pub action: Action, 
 }
 
 pub const KEYWORD_SESSION: &str = "session";
@@ -20,7 +29,7 @@ pub fn parse_args() ->Vec<Token>{
     let args = std::env::args();
 
     for arg in args {
-        result.push(generate_token(&arg));
+        result.push(generate_token(&arg.trim()));
     }
 
     result
@@ -28,15 +37,15 @@ pub fn parse_args() ->Vec<Token>{
 
 fn generate_token(arg: &str) -> Token {
     match arg {
-        "session" => Token{kind:Type::Keyword, str: KEYWORD_SESSION.to_string()},
-        "s" => Token{kind:Type::Keyword, str: KEYWORD_SESSION.to_string()},
-        "task" => Token{kind:Type::Keyword, str:KEYWORD_TASK.to_string()},
-        "t" => Token{kind:Type::Keyword, str: KEYWORD_TASK.to_string()},
+        "session" => Token{kind:Type::Keyword, str: KEYWORD_SESSION.to_string(),action:Action::Session},
+        "s" => Token{kind:Type::Keyword, str: KEYWORD_SESSION.to_string(), action:Action::Session},
+        "task" => Token{kind:Type::Keyword, str:KEYWORD_TASK.to_string(), action:Action::Task},
+        "t" => Token{kind:Type::Keyword, str: KEYWORD_TASK.to_string(), action:Action::Task},
         _ => {
             if arg.contains("-") || arg.contains("--") {
-                Token{kind:Type::Arg, str: arg.to_string()}
+                Token{kind:Type::Arg, str: arg.to_string(),action:Action::None}
             }else{
-                Token{kind:Type::Value, str: arg.to_string()}
+                Token{kind:Type::Value, str: arg.to_string(),action:Action::None}
             }
         }
     }
